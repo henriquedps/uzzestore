8768f673481a831848fabebf7b5e36c37870b0a2
henriquepds <henrique99429151@gmail.com>
2025-10-09 17:58:19 -0400

    yes

--- trocar_senha_admin.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from werkzeug.security import generate_password_hash
-
-conn = sqlite3.connect('loja.db')
-nova_senha = generate_password_hash("admin123")
-conn.execute("UPDATE usuarios SET senha = ? WHERE email = ?", (nova_senha, "admin@uzzerstore.com"))
-conn.commit()
-conn.close()
-print("Senha do admin atualizada!")
