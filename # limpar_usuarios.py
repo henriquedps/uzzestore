94205f1254084eb6784f3e685f896fcb6c14a566
henriquepds <henrique99429151@gmail.com>
2025-10-08 21:29:27 -0400

    yes

--- # limpar_usuarios.py
+++ # limpar_usuarios.py
@@ -9,13 +9,7 @@ conn.execute("DELETE FROM usuarios")
 conn.commit()
 
 # Criar admin padrão
-admin_email = "admin@uzzerstore.com"
-admin_senha = generate_password_hash("admin123")
-conn.execute(
-    "INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)",
-    ("Administrador", admin_email, admin_senha)
-)
+
 conn.commit()
 conn.close()
-
-print("Todos os usuários foram removidos e o admin foi recriado com a senha: admin123")
+print("Todos os usuários foram apagados ")
